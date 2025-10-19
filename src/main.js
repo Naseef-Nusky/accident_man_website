c210e786a37a242ec386f7a7ad8d12b90eabd604
Naseef Nusky <naseefnusky09@gmail.com>
2025-10-19 15:35:16 +0530

    Fix Router context error: wrap App with BrowserRouter at entry point (main.js)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root");
+createRoot(rootEl).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
