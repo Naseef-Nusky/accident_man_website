d40f485cf0cd232314df2548114fffc7c34773d6
Naseef Nusky <naseefnusky09@gmail.com>
2025-10-19 15:55:51 +0530

    Fix scroll to top: mount ScrollToTop within layout so it runs on route changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header.js";
 import { Footer } from "./components/Footer.js";
+import { ScrollToTop } from "./components/ScrollToTop.js";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </div>
