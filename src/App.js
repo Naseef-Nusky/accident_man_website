8cad12a290ce0c9828a935e144d21cb1d1f273cb
Naseef Nusky <naseefnusky09@gmail.com>
2025-10-19 15:42:20 +0530

    Router v7 prep: switch to RouterProvider/createBrowserRouter with future flags; use Outlet in App

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header.js";
 import { Footer } from "./components/Footer.js";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import AboutPage from "./pages/About.js";
-import Accident from "./pages/Accident.js";
-import ReplacementVehicles from "./pages/ReplacementVehicles.js";
-import BodyshopRepairs from "./pages/BodyshopRepairs.js";
-import Contact from "./pages/Contact.js";
-import CreditHire from "./pages/CreditHire.js";
-import Blogs from "./pages/Blogs.js";
-import BlogPostRoute from "./pages/BlogPostRoute.js";
-import { Terms } from "./pages/Terms.js";
-import { Complaints } from "./pages/Complaints.js";
-import { PrivacyPolicy } from "./pages/PrivacyPolicy.js";
-import NotFound from "./pages/NotFound.js";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/accident" element={<Accident />} />
-        <Route path="/replacement-vehicles" element={<ReplacementVehicles />} />
-        <Route path="/credit-hire" element={<CreditHire />} />
-        <Route path="/bodyshop-repairs" element={<BodyshopRepairs />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:slug" element={<BlogPostRoute />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/complaints" element={<Complaints />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Outlet />
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutPage from './pages/About';
+import Accident from './pages/Accident';
+import ReplacementVehicles from './pages/ReplacementVehicles';
+import BodyshopRepairs from './pages/BodyshopRepairs';
+import Contact from './pages/Contact';
+import CreditHire from './pages/CreditHire';
+import Blogs from './pages/Blogs';
+import BlogPostRoute from './pages/BlogPostRoute';
+import { Terms } from './pages/Terms';
+import { Complaints } from './pages/Complaints';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    future: { v7_startTransition: true, v7_relativeSplatPath: true },
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'about', element: <AboutPage /> },
+      { path: 'accident', element: <Accident /> },
+      { path: 'replacement-vehicles', element: <ReplacementVehicles /> },
+      { path: 'credit-hire', element: <CreditHire /> },
+      { path: 'bodyshop-repairs', element: <BodyshopRepairs /> },
+      { path: 'blogs', element: <Blogs /> },
+      { path: 'blogs/:slug', element: <BlogPostRoute /> },
+      { path: 'contact', element: <Contact /> },
+      { path: 'terms', element: <Terms /> },
+      { path: 'complaints', element: <Complaints /> },
+      { path: 'privacy-policy', element: <PrivacyPolicy /> },
+      { path: '*', element: <NotFound /> }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} future={{ v7_startTransition: true, v7_relativeSplatPath: true }} />
   </React.StrictMode>
 );
 
